860cf7a915a17d6979427d70b4dcbf8e4c7e6225
NofarZiv <nofarziv13@gmail.com>
2024-07-11 11:54:39 -0230

    add register, login and logout options.

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,12 @@ Rails.application.routes.draw do
     resources :categories
   end
 
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   get "/about", to: "about#index"
 
